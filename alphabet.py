817b7ff3d10867313d7f24a6c3e3222740106d18
parthasmart <35663420+parthasmart@users.noreply.github.com>
2018-01-24 17:01:49 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,10 @@
+while crt:
+print("enter '0' for exit:")
+ch=input("enter any character:")
+if(ch=='0')
+break
+else:
+if((ch>='a' and ch<='z') or(ch>='A' and ch<='Z')):
+print("alphabet")
+else:
+print("not a alphabet")
