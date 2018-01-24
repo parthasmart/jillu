09db29563f076eb93a3d45833198257dc0f2a67c
parthasmart <35663420+parthasmart@users.noreply.github.com>
2018-01-24 17:17:06 +0530

    Create largestnumber.py

---
+++ largestnumber.py
@@ -0,0 +1,12 @@
+num1=float(input("enetr first number:"))
+num2=float(input("enetr second number:"))
+num3=float(input("enetr third number:"))
+if(num1>=num2) and (num1>=num3):
+largest=num1
+elif(num2>=num1) and (num 2>=num3):
+largest =num2
+else:
+largest=num3
+print("the largest number",num1,",",num2,"and",num3,"is",largest)
+
+
