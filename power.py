567bb979836607118a65550d939885057abc203b
parthasmart <35663420+parthasmart@users.noreply.github.com>
2018-01-28 20:10:38 +0530

    Create power.py

---
+++ power.py
@@ -0,0 +1,9 @@
+base=int(input("enter the base number: ")
+exp=int(input("enter the exp number : ")
+def power(base,exp):
+res=1
+for i in range(exp)
+res=res*base
+print res
+return res
+print power(base,exp)
